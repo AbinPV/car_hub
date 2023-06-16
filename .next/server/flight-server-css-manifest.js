883e7f176e0f1f0045f964b492badf0cf53bf299
Abin Pallichanathu Varghese <54523595+AbinPV@users.noreply.github.com>
2023-06-17 01:36:29 +0530

    care rent app

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,12 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\Users\\Abin\\Downloads\\car_hub\\app\\layout.tsx": [
+      "C:\\Users\\Abin\\Downloads\\car_hub\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\Users\\Abin\\Downloads\\car_hub\\app\\page": [
+      "C:\\Users\\Abin\\Downloads\\car_hub\\app\\globals.css"
+    ]
+  }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Error Next is not found
+
+To solve the error, run "npm install react react-dom next" to install next
+
+
+## or start buy using 
+
+ to run app
+ 
+"npm run dev"
+
+
